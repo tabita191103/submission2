fcce5a39a83c235566fd81147886b8462134d599
Tabita Novi Sinaga <135198172+tabita191103@users.noreply.github.com>
2025-10-15 15:31:33 +0700

    Update login.cy.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,6 +5,7 @@
  *   - should display alert when password is empty
  *   - should display alert when email and password are wrong
  *   - should display homepage when email and password are correct
+ *   - should intentionally fail to trigger CI error
  */
 
 describe('Login spec', () => {
@@ -22,7 +23,7 @@ describe('Login spec', () => {
   it('should display alert when email is empty', () => {
     // klik tombol login tanpa mengisi username
     cy.get('button').contains(/^Login$/).click();
- 
+
     // memverifikasi window.alert untuk menampilkan pesan dari API
     cy.on('window:alert', (str) => {
       expect(str).to.equal('"email" is not allowed to be empty');
@@ -32,10 +33,10 @@ describe('Login spec', () => {
   it('should display alert when password is empty', () => {
     // mengisi username
     cy.get('input[placeholder="email@example.com"]').type('email@example.com');
- 
+
     // klik tombol login tanpa mengisi password
     cy.get('button').contains(/^Login$/).click();
- 
+
     // memverifikasi window.alert untuk menampilkan pesan dari API
     cy.on('window:alert', (str) => {
       expect(str).to.equal('"password" is not allowed to be empty');
@@ -45,13 +46,13 @@ describe('Login spec', () => {
   it('should display alert when email and password are wrong', () => {
     // mengisi username
     cy.get('input[placeholder="email@example.com"]').type('email@example.com');
- 
+
     // mengisi password yang salah
     cy.get('input[placeholder="enter your password"]').type('wrong_password');
- 
+
     // menekan tombol Login
     cy.get('button').contains(/^Login$/).click();
- 
+
     // memverifikasi window.alert untuk menampilkan pesan dari API
     cy.on('window:alert', (str) => {
       expect(str).to.equal('email or password is wrong');
@@ -61,18 +62,23 @@ describe('Login spec', () => {
   it('should display homepage when email and password are correct', () => {
     // mengisi username
     cy.get('input[placeholder="email@example.com"]').type('abdullah@dicoding.com');
- 
+
     // mengisi password
     cy.get('input[placeholder="enter your password"]').type('abdullah');
- 
+
     // menekan tombol Login
     cy.get('button').contains(/^Login$/).click();
- 
+
     // memverifikasi bahwa elemen yang berada di homepage ditampilkan
     cy.get('header').contains(/^Forum App$/).should('be.visible');
 
     // Menekan tombol dropdown untuk melihat menu Sign out
     cy.get('.nav-profile').click();
     cy.get('.dropdown-item').contains(/^Sign out$/).should('be.visible');
+  });
+
+  // 🧨 Test ini dibuat untuk memicu kegagalan CI
+  it('should intentionally fail to trigger CI error', () => {
+    expect(true).to.equal(false);
   });
 });
